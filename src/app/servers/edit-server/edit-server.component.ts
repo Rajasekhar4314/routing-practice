cc2eae97f7c16820e9d6ad0536fefe18a3f325bf
Rajasekhar4314 <rajasekhar4314@gmail.com>
2025-07-24 10:15:53 +0530

    retriving query params

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { ServersService } from './../servers.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,12 +11,21 @@ export class EditServerComponent implements OnInit {
   server : any;
   serverName !: string;
   serverStatus !: string;
+  queryparams :  any;
+  fragment : any;
 
   constructor(
-    private serversService : ServersService
+    private serversService : ServersService,
+    private route : ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    console.log("qury parmas ", this.route.snapshot.queryParams)
+    console.log("fragment is :", this.route.snapshot.fragment)
+    this.queryparams = this.route.queryParams.subscribe()
+    this.fragment = this.route.fragment.subscribe()
+
+    console.log("query :", this.queryparams, this.fragment);
     this.server = this.serversService.getServer(1)
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
@@ -27,5 +37,5 @@ export class EditServerComponent implements OnInit {
   }
 
 
-
+  
 }
