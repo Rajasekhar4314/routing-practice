99fa0055d84109a4eb927b4c2c7f3be067f1cfb9
Rajasekhar4314 <rajasekhar4314@gmail.com>
2025-07-26 15:55:19 +0530

    added app.routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,25 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes : Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children : [
-    { path: ':id/:name', component: UserComponent },
-  ] },
-  { path: 'servers', component: ServersComponent, 
-    children : [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ]
-  },
-  // wild card path
-  {path: '**', component : PageNotFoundComponent},
-  // {path: '**', redirectTo : 'not-found'}
-
-
-
-]
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -47,9 +29,10 @@ const appRoutes : Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ ServersService ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
